46ba941ef4bee7dc4564fcd08e4f922c6831340a
parathantl <parathanlive123@gmail.com>
2025-02-06 22:14:42 +0530

    fix: fix build command

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://distribution-mgt-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
